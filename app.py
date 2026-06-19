d990f3329ffa0865b667d14fee9b783c2d1d6055
Manasa Vegi <manuvegi78@gmail.com>
2026-06-19 22:37:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 import streamlit as st
 
+from src.classifier import classify_persona
+from src.generator import generate_response
+from src.escalator import should_escalate
+
 st.title("Persona Support Agent")
 
-user_input = st.text_area("Enter your query")
+query = st.text_area("Enter your query")
 
 if st.button("Submit"):
-    st.write("Response will appear here")
+
+    persona = classify_persona(query)
+
+    if should_escalate(query):
+        st.error("Escalation Required")
+    else:
+        response = generate_response(persona, query)
+
+        st.write("### Persona")
+        st.write(persona)
+
+        st.write("### Response")
+        st.write(response)
